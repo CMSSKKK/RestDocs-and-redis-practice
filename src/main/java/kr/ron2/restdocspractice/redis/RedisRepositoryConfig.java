feb65647b1fdb3a273370a4e20f465dfb0bcab69
Minseok-Choi <cms05041@gmail.com>
2022-08-18 23:52:16 +0900

    feat: Stock 엔티티 구현

--- src/main/java/kr/ron2/restdocspractice/redis/RedisRepositoryConfig.java
+++ src/main/java/kr/ron2/restdocspractice/redis/RedisRepositoryConfig.java
@@ -8,8 +8,6 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableRedisRepositories
---
+++ src/main/java/kr/ron2/restdocspractice/stock/Stock.java
@@ -0,0 +1,33 @@
+package kr.ron2.restdocspractice.stock;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class Stock {
+
+    @Id
+    private Long id;
+    private String name;
+    private Long quantity;
+
+    public static Stock of(String name, Long quantity) {
+        return new Stock(null, name, quantity);
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity - quantity < 0) {
+            throw new IllegalArgumentException("재고 부족");
+        }
+        this.quantity -= quantity;
+    }
+
+}
